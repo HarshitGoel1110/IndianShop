39ff23aa2104b967b74686a7109795f5894d205e
Ritesh <ritesh.somani2018@vitstudent.ac.in>
2020-06-22 00:37:16 +0530

    another commit5

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const list=document.getElementById('displaystores');
-const addShop=(store)=>{
+
+function setItem1(id)
+{
+	console.log("called");
+	window.localStorage.setItem("id",id);
+}
+
+const addShop=(store,doc)=>{
 	let html=`
     <div class="p-4 md:w-1/3">
     <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
@@ -9,7 +16,7 @@ const addShop=(store)=>{
         <h1 class="title-font text-lg font-medium text-gray-900 mb-3">${store.name}</h1>
         <p class="leading-relaxed mb-3">${store.description}</p>
         <div class="flex items-center flex-wrap ">
-          <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Explore this store 
+          <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" href='pro.html?name=${doc}')>Explore this store 
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
@@ -21,12 +28,17 @@ const addShop=(store)=>{
     </div>
   </div>
 	`;
-	console.log(html);
+	console.log(doc);
+	
+	// console.log(html);
 	list.innerHTML +=html;
 }
+
+
+
 db.collection('shop').get().then((snapshot) =>{
 	snapshot.docs.forEach(doc=>{
-		addShop(doc.data());
+		addShop(doc.data(),doc.id);
 	})
 
 }).catch(err=>{
---
+++ pro.js
@@ -0,0 +1,45 @@
+var url = document.location.href;
+
+const lt=document.getElementById('storename');
+
+
+
+const list=document.getElementById('displayproducts');
+
+var qw="";
+for(var i=url.length-1;i>=0;i--)
+{   
+    if(url[i]==='=')break;
+    qw=url[i]+qw;
+}
+console.log(qw);
+db.collection('shop').doc(qw).get().then(doc=>{
+	let ht=`<h1 class="sm:text-4xl text-2xl font-medium title-font mb-2 text-gray-900">${doc.data().name}</h1>`;
+	lt.innerHTML += ht;
+}
+)
+const addProduct=(product)=>{
+	let html=`
+    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a class="block relative h-48 rounded overflow-hidden">
+        <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268">
+        </a>
+        <div class="mt-4">
+        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+        <h2 class="text-gray-900 title-font text-lg font-medium">${product.name}</h2>
+        <p class="mt-1">₹ ${product.price}</p>
+        </div>
+    </div>
+	`;
+	list.innerHTML +=html;
+}
+
+db.collection('shop/'+qw+'/product').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        addProduct(doc.data());
+    })
+}).catch(err => {
+    console.log(err);
+});
+
+console.log("rit");

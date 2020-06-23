bfec72eb2ed60944a562d1b5fcb1a3f235652ae6
Ritesh <ritesh.somani2018@vitstudent.ac.in>
2020-06-23 10:30:34 +0530

    another commit11

--- auth.js
+++ auth.js
@@ -1,33 +1,34 @@
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
-    if (user) {
-        setupUI(user);
-    } else {
-      setupUI();
-    }
-  })
-  
+  if (user) {
+      setupUI(user);
+  } else {
+    setupUI();
+  }
+})
+
 // create new store
 const createForm = document.querySelector('#create-form');
 createForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  db.collection('shop').add({
-    name: createForm.storename.value,
-    firstname: createForm.firstname.value,
-    lastname: createForm.lastname.value,
-    address: createForm.address.value,
-    city: createForm.city.value,
-    country: createForm.country.value,
-    state: createForm.state.value,
-    pincode: createForm.pincode.value,
-    mobile: createForm.mobileno.value,
-    email: createForm.email.value,
-    isreg: createForm.isreg.value
-  }).then(() => {
-    createForm.reset();
-  }).catch(err => {
-    console.log(err.message);
-  });
+e.preventDefault();
+const store = {
+  name: createForm['storename'].value,
+  firstname: createForm['firstname'].value,
+  lastname: createForm['lastname'].value,
+  address: createForm['address'].value,
+  city: createForm['city'].value,
+  country: createForm['country'].value,
+  state: createForm['state'].value,
+  pincode: createForm['pincode'].value,
+  mobile: createForm['mobileno'].value,
+  email: createForm['email'].value,
+  isreg: createForm['isreg'].value
+}
+db.collection('shop').add(store).then(() => {
+  createForm.reset();
+}).catch(err => {
+  console.log(err.message);
+});
 });
 
 // signup
@@ -39,37 +40,43 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user
+  // sign up the user & add firestore data
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-
+    return db.collection('users').doc(cred.user.uid).set({
+      name: signupForm['signup-name'].value,
+      mobileno: signupForm['signup-mobileno'].value
+    });
+  }).then(() => {
+    // close the signup modal & reset form
     signupForm.reset();
   });
 });
 
 
+
 // logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut().then(() => {
-    console.log('user signed out');
-  })
+e.preventDefault();
+auth.signOut().then(() => {
+  console.log('user signed out');
+})
 });
 
 
 // login
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
+e.preventDefault();
 
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    
-    loginForm.reset();
-  });
+// get user info
+const email = loginForm['login-email'].value;
+const password = loginForm['login-password'].value;
+
+// log the user in
+auth.signInWithEmailAndPassword(email, password).then((cred) => {
+  
+  loginForm.reset();
+});
 
 });
--- header.js
+++ header.js
@@ -4,21 +4,25 @@ const accountDetails = document.querySelector('.account-details');
 
 
 const setupUI = (user) => {
-    if (user) {
-      // account info
+  if (user) {
+    // account info
+    db.collection('users').doc(user.uid).get().then(doc => {
       const html = `
         <div>Logged in as ${user.email}</div>
+        <div>Name: ${doc.data().name}</div>
+        <div>Mobile No: ${doc.data().mobileno}</div>
       `;
       accountDetails.innerHTML = html;
-      // toggle user UI elements
-      loggedInLinks.forEach(item => item.style.display = 'block');
-      loggedOutLinks.forEach(item => item.style.display = 'none');
-    } else {
-      // clear account info
-      accountDetails.innerHTML = '';
-      // toggle user elements
-      loggedInLinks.forEach(item => item.style.display = 'none');
-      loggedOutLinks.forEach(item => item.style.display = 'block');
-    }
-  };
+    });
+    // toggle user UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    // clear account info
+    accountDetails.innerHTML = '';
+    // toggle user elements
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+};
   

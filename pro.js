22468e9934f2d3d0e660f6c08f922ded0987e057
Ritesh <ritesh.somani2018@vitstudent.ac.in>
2020-07-03 21:08:21 +0530

    imgrs

--- pro.js
+++ pro.js
@@ -45,7 +45,7 @@ function dispProduct(product,idpro,user)
     html=`
     <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
         <a class="block relative h-48 rounded overflow-hidden">
-        <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="${product.image}">
+        <img alt="ecommerce"  class="object-contain object-center w-full h-full block" src="${product.image}">
         </a>
         <div class="mt-4">
         <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">${product.description}</h3>
